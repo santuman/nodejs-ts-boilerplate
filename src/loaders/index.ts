ee0f894cc2f71ce617c916a36475beae99976f00
Santosh Subedi <santsubcode@gmail.com>
2021-10-16 19:11:42 +0545

    🐛 FIX: remove console log for mongoURI

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -5,9 +5,6 @@ const loader = async () => {
 	/**
 	 * Database Connection
 	 */
-	console.log({
-		mongoURI: `${keys.DB_PROTOCOL}://${keys.DB_USER}:${keys.DB_PASSWORD}@${keys.DB_HOST}:${keys.DB_PORT}/${keys.DB_NAME}?${keys.DB_PARAMS}`,
-	})
 	const mongoURI = `${keys.DB_PROTOCOL}://${keys.DB_USER}:${keys.DB_PASSWORD}@${keys.DB_HOST}:${keys.DB_PORT}/${keys.DB_NAME}?${keys.DB_PARAMS}`
 	await connectMongoDB(mongoURI)
 }
