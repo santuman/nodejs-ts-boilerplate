250473031329446a4c874710b3f049e948911374
Santosh Subedi <santsubcode@gmail.com>
2021-10-16 19:09:52 +0545

    🐛 FIX: eslint quotes issue fixed

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -6,7 +6,7 @@ const envFound = dotenv.config({
 })
 
 if (envFound.error) {
-	throw new Error('⚠️ Couldn\'t find .env file ⚠️')
+	throw new Error("⚠️ Couldn't find .env file ⚠️")
 }
 
 const keys = {
